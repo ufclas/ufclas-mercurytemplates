583f8b3d85c93dfe57ae08ce89a3412362794ceb
suisrael <88337104+suisrael@users.noreply.github.com>
2025-01-02 12:29:27 -0500

    clean up spacing, try removing .php

--- ufclas-mercurytemplates.php
+++ ufclas-mercurytemplates.php
@@ -7,43 +7,29 @@
  * Author:            Suzie Israel
  */
 
-function my_template_array () 
-{
-		$temps = [];
-
-		$temps['custom-post-archive.php'] = 'Custom Post Archive';
-
-		return $temps;
+function my_template_array () {
+	$temps = [];
+	$temps['custom-post-archive'] = 'Custom Post Archive';
+	return $temps;
 }
 
-function my_template_register($page_templates,$theme,$post) 
-{
-
+function my_template_register($page_templates,$theme,$post) {
 	$templates = my_template_array();
-
-	foreach($templates as $tk=>$tv) 
-	{
+	foreach($templates as $tk=>$tv) {
 		$page_templates[$tk] = $tv;
 	}
-
 	return $page_templates;
 }
 add_filter('theme_page_templates','my_template_register',10,3);
 
-function my_template_select($template)
-{
+function my_template_select($template) {
 	global $post,$wp_query,$wpdb;
-
 	$page_temp_slug = get_page_template_slug( $post->ID );
-
 	$templates = my_template_array();
 
-
-	if(isset($templates[$page_temp_slug]))
-	{
+	if(isset($templates[$page_temp_slug])) {
 			$template = plugin_dir_path(__FILE__).'templates/'.$page_temp_slug;
 	}
-
 	return $template;
 }
 
