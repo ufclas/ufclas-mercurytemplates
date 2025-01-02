2616c27c53b8418fd0e3b25a805fc2411a1c0256
suisrael <88337104+suisrael@users.noreply.github.com>
2025-01-02 07:08:49 -0500

    remove prefix from category title

--- ufclas-mercurytemplates.php
+++ ufclas-mercurytemplates.php
@@ -196,4 +196,11 @@ function misha_filter_function_custom(){
 	die();
 }
 
+function prefix_category_title( $title ) {
+	if ( is_category() ) {
+	$title = single_cat_title( '', false );
+	}
+	return $title;
+	}
+	add_filter( 'get_the_archive_title', 'prefix_category_title' );
   ?>
