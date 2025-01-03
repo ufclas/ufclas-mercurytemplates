bef7e65f207761835a14ae385a076807e1a1b1bd
suisrael <88337104+suisrael@users.noreply.github.com>
2025-01-02 21:07:10 -0500

    try resetting the parameters

--- templates/custom-post-archive.php
+++ templates/custom-post-archive.php
@@ -92,11 +92,13 @@ $postid = get_option('page_for_posts');
     <div id="misha_posts_wrap" class="row position-relative news-row" data-masonry="{&quot;percentPosition&quot;: true }">
       <?php
 
+      wp_reset_query();
+
       $selected_category_slug = get_post_meta(get_the_ID(), 'selected-category', true);
 
       $params = [
-        "posts_per_page" => 15,
-        "category_name" => "$selected_category_slug",
+          "posts_per_page" => 15,
+          "category_name" => $selected_category_slug,
       ];
 
       query_posts($params);
