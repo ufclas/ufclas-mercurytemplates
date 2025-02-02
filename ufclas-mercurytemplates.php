60c0f1dc06adfe8f80faf16981c37ccc1e46ceb0
suisrael <88337104+suisrael@users.noreply.github.com>
2025-02-02 12:03:37 -0500

    fix "0" in body of page

--- ufclas-mercurytemplates.php
+++ ufclas-mercurytemplates.php
@@ -120,22 +120,23 @@ function my_custom_archive_template($archive_template) {
 
 add_action( 'wp_enqueue_scripts', 'misha_script_and_styles_2');
 
-function misha_script_and_styles_2() {
-	// absolutely need it, because we will get $wp_query->query_vars and $wp_query->max_num_pages from it.
-	global $wp_query;
-	// when you use wp_localize_script(), do not enqueue the target script immediately
-	wp_register_script( 'misha_scripts', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins/ufclas-mercurytemplates/js/ajax-script.js', array('jquery') );;
-	// passing parameters here
-	// actually the <script> tag will be created and the object "misha_loadmore_params" will be inside it 
-	wp_localize_script( 'misha_scripts', 'misha_loadmore_params', array(
-		'ajaxurl' => include($_SERVER['DOCUMENT_ROOT']) . '/wp-admin/admin-ajax.php', // WordPress AJAX
-		'posts' => json_encode( $wp_query->query_vars ), // everything about your loop is here
-		'current_page' => $wp_query->query_vars['paged'] ? $wp_query->query_vars['paged'] : 1,
-		'max_page' => $wp_query->max_num_pages
-	) );
- 	wp_enqueue_script( 'misha_scripts' );
-}
+add_action( 'wp_enqueue_scripts', 'misha_script_and_styles_2');
 
+function misha_script_and_styles_2() {
+    // absolutely need it, because we will get $wp_query->query_vars and $wp_query->max_num_pages from it.
+    global $wp_query;
+    // when you use wp_localize_script(), do not enqueue the target script immediately
+    wp_register_script( 'misha_scripts', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins/ufclas-mercurytemplates/js/ajax-script.js', array('jquery') );
+    // passing parameters here
+    // actually the <script> tag will be created and the object "misha_loadmore_params" will be inside it 
+    wp_localize_script( 'misha_scripts', 'misha_loadmore_params', array(
+        'ajaxurl' => admin_url('admin-ajax.php'), // WordPress AJAX
+        'posts' => json_encode( $wp_query->query_vars ), // everything about your loop is here
+        'current_page' => $wp_query->query_vars['paged'] ? $wp_query->query_vars['paged'] : 1,
+        'max_page' => $wp_query->max_num_pages
+    ) );
+    wp_enqueue_script( 'misha_scripts' );
+}
 add_action('wp_ajax_loadmorebutton', 'misha_loadmore_ajax_handler_2');
 add_action('wp_ajax_nopriv_loadmorebutton', 'misha_loadmore_ajax_handler_2');
  
